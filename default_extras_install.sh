fe3d0ab6efd49358248233dd58f2a00be57a58d5
Autumn Ashton <misyl@froggi.es>
2025-06-17 01:40:46 +0100

    looks: Add some default looks, and install them

--- default_extras_install.sh
+++ default_extras_install.sh
@@ -3,4 +3,6 @@
 # Remove old Gamescope default configs and add our own.
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gamescope"
 rm -rf "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gamescope/scripts" || true
+rm -rf "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gamescope/looks" || true
 cp -r "${MESON_SOURCE_ROOT}/scripts" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gamescope/scripts"
+cp -r "${MESON_SOURCE_ROOT}/looks" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gamescope/looks"
